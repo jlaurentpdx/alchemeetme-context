ced176fa3c833907361d264c6fd35abf79c7a2cd
Jordan Laurent <jlaurent.pdx@gmail.com>
2022-02-07 13:13:22 -0800

    replaced user state in App with UserContext

--- src/context/UserContext.jsx
+++ src/context/UserContext.jsx
@@ -1,9 +1,20 @@
-import { useState, useContext, createContext } from 'react'
+import { useState, useEffect, useContext, createContext } from 'react'
+import fetchUser from '../services/user'
 
 const UserContext = createContext()
 
 const UserProvider = ({ children }) => {
   const [user, setUser] = useState({})
+
+  useEffect(() => {
+    fetchUser()
+      .then((fetchedUser) => {
+        setUser(fetchedUser)
+      })
+      .catch((error) => {
+        throw new Error(`Error: ${error}`)
+      })
+  }, [])
 
   return <UserContext.Provider value={{ user, setUser }}>{children}</UserContext.Provider>
 }
