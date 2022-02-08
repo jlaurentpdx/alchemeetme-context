56fa9338ce2317d429191ff957f99d6aba675732
Jordan Laurent <jlaurent.pdx@gmail.com>
2022-02-08 09:26:03 -0800

    moved useUser from Layout to Header

--- src/views/Layout/Layout.jsx
+++ src/views/Layout/Layout.jsx
@@ -1,13 +1,10 @@
 import Footer from '../../components/layout/Footer'
 import Header from '../../components/layout/Header'
-import { useUser } from '../../context/UserContext'
 
 const Layout = ({ children }) => {
-  const { user } = useUser()
-
   return (
     <div className="min-h-screen flex flex-col">
-      <Header user={user} />
+      <Header />
       <main className="flex flex-col flex-1 justify-center items-center">{children}</main>
       <Footer />
     </div>
